32711795780b7986c3dda9d125f5a375456f97f9
chiaracarlino <chiara.carlino@epfedu.fr>
2025-04-27 14:38:44 +0200

    Ajout image dans config

--- src/main/java/com/epf/config/WebConfig.java
+++ src/main/java/com/epf/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 @EnableWebMvc
@@ -19,5 +20,11 @@ public class WebConfig implements WebMvcConfigurer {
             .maxAge(3600)  // Cache CORS config for 1 hour
             .allowCredentials(true);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations("/images/");
+}
 }
 
